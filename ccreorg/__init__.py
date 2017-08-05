51eabde26162125de0fbee096f173c3bf1f4cca2
Andrew M. James <amjames2@vt.edu>
2017-08-04 20:26:26 -0400

    fix naming in python sources

--- ccreorg/__init__.py
+++ ccreorg/__init__.py
@@ -37,9 +37,7 @@ __author__  = 'Psi4 Developer'
 from .pymodule import *
 
 # Load C++ plugin
-import os
 import psi4
-plugdir = os.path.split(os.path.abspath(__file__))[0]
-sofile = plugdir + '/' + os.path.split(plugdir)[1] + '.so'
-psi4.core.plugin_load(sofile)
+from core import PLUGIN_SOFILE
+psi4.core.plugin_load(PLUGIN_SOFILE)
 
